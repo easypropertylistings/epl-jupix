ab180ba2098bab0ac6d49c05a349aee1e5e6b07d
mervb1@gmail.com <mervb1@gmail.com>
2020-03-17 10:26:54 +0800

    Beautify

--- includes/hooks.php
+++ includes/hooks.php
@@ -1,16 +1,16 @@
 <?php
-function epl_jpi_license_options_filter($fields = null) {
+function epl_jpi_license_options_filter( $fields = null ) {
 	$fields[] = array(
-		'label'		=>	'',
-		'fields'	=>	array(
+		'label'  => '',
+		'fields' => array(
 			array(
-				'name'	=>	'jupix_integration',
-				'label'	=>	'JUPIX Integration license key',
-				'type'	=>	'text'
-			)
-		)
+				'name'  => 'jupix_integration',
+				'label' => 'JUPIX Integration license key',
+				'type'  => 'text',
+			),
+		),
 	);
-	
+
 	return $fields;
 }
-add_filter('epl_license_options_filter', 'epl_jpi_license_options_filter', 10, 3);
+add_filter( 'epl_license_options_filter', 'epl_jpi_license_options_filter', 10, 3 );
--- includes/template-functions.php
+++ includes/template-functions.php
@@ -10,7 +10,9 @@
  */
 
 // Exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 
 
@@ -18,86 +20,79 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 /**
  * Sold STC Pricing Sticker
  *
- * @return		If the listing meta property_sold_stc is yes, return the label.
- *
+ * @return      If the listing meta property_sold_stc is yes, return the label.
  */
 function epl_jupix_sold_stc_sticker( $price_sticker ) {
 
 	global $property;
 
-	if ( 'yes' == $property->get_property_meta('property_sold_stc') && 'sold' != $property->get_property_meta('property_status') ) {
-		$price_sticker = '';
-		$label = __( 'Sold STC' , 'epl-jupix' );
-		$price_sticker .= '<span class="status-sticker under-offer sold-stc">'.$label.'</span>';
+	if ( 'yes' == $property->get_property_meta( 'property_sold_stc' ) && 'sold' != $property->get_property_meta( 'property_status' ) ) {
+		$price_sticker  = '';
+		$label          = __( 'Sold STC', 'epl-jupix' );
+		$price_sticker .= '<span class="status-sticker under-offer sold-stc">' . $label . '</span>';
 	}
 
 	return $price_sticker;
 }
-add_filter( 'epl_get_price_sticker' , 'epl_jupix_sold_stc_sticker' );
+add_filter( 'epl_get_price_sticker', 'epl_jupix_sold_stc_sticker' );
 
 
 /**
  * Property Kitchens
  *
  * Add kitchens to additional features list
- *
  */
- function epl_jupix_additional_feature_kitchens( $array ) {
+function epl_jupix_additional_feature_kitchens( $array ) {
 
-	$array[] = 'property_kitchens'; //whatever your new field name is
+	$array[] = 'property_kitchens'; // whatever your new field name is
 	return $array;
 }
-add_filter( 'epl_property_additional_features_list' , 'epl_jupix_additional_feature_kitchens' );
+add_filter( 'epl_property_additional_features_list', 'epl_jupix_additional_feature_kitchens' );
 
 /**
  * Property Reception Rooms
  *
  * Add Reception Rooms to additional features list
- *
  */
- function epl_jupix_additional_feature_reception_rooms( $array ) {
+function epl_jupix_additional_feature_reception_rooms( $array ) {
 
-	$array[] = 'property_reception_rooms'; //whatever your new field name is
+	$array[] = 'property_reception_rooms'; // whatever your new field name is
 	return $array;
 }
-add_filter( 'epl_property_additional_features_list' , 'epl_jupix_additional_feature_reception_rooms' );
+add_filter( 'epl_property_additional_features_list', 'epl_jupix_additional_feature_reception_rooms' );
 
 
 /**
  * Disable Features Taxonomy Link
- *
  */
 function epl_jupix_disable_feature_links() {
 	return false;
 }
-add_filter( 'epl_features_taxonomy_link_filter' , 'epl_jupix_disable_feature_links' );
+add_filter( 'epl_features_taxonomy_link_filter', 'epl_jupix_disable_feature_links' );
 
 
 
 /**
  * Rename Tour button to Brochure
- *
  */
 function epl_jupix_rename_tour() {
-	return __( 'Brochure' , 'epl-jupix' );
+	return __( 'Brochure', 'epl-jupix' );
 }
-add_filter( 'epl_button_label_tour' , 'epl_jupix_rename_tour' );
-add_filter( 'epl_button_label_property_external_link_1' , 'epl_jupix_rename_tour' );
+add_filter( 'epl_button_label_tour', 'epl_jupix_rename_tour' );
+add_filter( 'epl_button_label_property_external_link_1', 'epl_jupix_rename_tour' );
 
 /**
  * Rename Tour button to Brochure
- *
  */
 function epl_jupix_rename_tour_second() {
-	return __( 'Virtual Tour' , 'epl-jupix' );
+	return __( 'Virtual Tour', 'epl-jupix' );
 }
-add_filter( 'epl_button_label_property_external_link_2' , 'epl_jupix_rename_tour_second' );
+add_filter( 'epl_button_label_property_external_link_2', 'epl_jupix_rename_tour_second' );
 
 
 
 /**
  * Rental Availability Price Prefix
- *
  */
 function epl_jupix_property_rental_availability_price_prefix( $price ) {
 
@@ -109,28 +104,26 @@ function epl_jupix_property_rental_availability_price_prefix( $price ) {
 			return $price;
 		}
 
-		$availability = $property->get_property_meta('property_rental_availability');
+		$availability = $property->get_property_meta( 'property_rental_availability' );
 
 		if ( $availability == '5' || $availability == '1' ) {
 			return $price;
 		}
 
-
 		$availability_options = array(
-			'1'	=>	__( 'On Hold' , 		'epl-jupix' ),
-			'2'	=>	__( 'To Let' , 			'epl-jupix' ),
-			'3'	=>	__( 'References Pending' , 	'epl-jupix' ),
-			'4'	=>	__( 'Let Agreed' , 		'epl-jupix' ),
-			'5'	=>	__( 'Let' , 			'epl-jupix' ),
-			'6'	=>	__( 'Withdrawn' , 		'epl-jupix' ),
+			'1' => __( 'On Hold', 'epl-jupix' ),
+			'2' => __( 'To Let', 'epl-jupix' ),
+			'3' => __( 'References Pending', 'epl-jupix' ),
+			'4' => __( 'Let Agreed', 'epl-jupix' ),
+			'5' => __( 'Let', 'epl-jupix' ),
+			'6' => __( 'Withdrawn', 'epl-jupix' ),
 		);
 
-		$avail_text = $availability_options[$availability];
+		$avail_text = $availability_options[ $availability ];
 
 	}
 
-
 	return '<span class="epl-jupix-rental-availability">' . $avail_text . '</span> ' . $price;
 }
-add_filter('epl_get_price', 'epl_jupix_property_rental_availability_price_prefix' );
+add_filter( 'epl_get_price', 'epl_jupix_property_rental_availability_price_prefix' );
 
